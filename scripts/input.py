cfb9bf93627f817798a233a4d4dbe5a5f3e0ffe6
Jake <me@jakesyl.com>
2015-03-28 18:51:01 -0400

    Update input.py

--- scripts/input.py
+++ scripts/input.py
@@ -3,27 +3,26 @@ Jake Sylvestre
 Twitter Utility
 Gets Tweets off Twitter using a skiddie-import strips them of all unnecesary information
 '''
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 
-from TwitterAPI import TwitterAPI
-import time
+ckey = '8VycHXMlsgdQwJosnxAuCNfal'
+csecret = 'eNaDNLL6EhfSG6fUfzVyeiOdPcOqB3teVpd4dVyjghwmJk6hfs'
+atoken = '2725326884-qM16ZMCOJw1Sdoi6jBrd0c6VAQKi4fykf8X5DaS'
+asecret = 'GlFQ0ZE3FDSiokSaVLj08qq0tmCw8sZjmwcai1oojJgbx'
 
-def get_tweets(user):
-	f = open('keys.txt', 'r')
-	lines = f.readlines()
-	tweets = []
-	api = TwitterAPI(consumer_key = '8VycHXMlsgdQwJosnxAuCNfal',
-	                  consumer_secret ='eNaDNLL6EhfSG6fUfzVyeiOdPcOqB3teVpd4dVyjghwmJk6hfs',
-	                  access_token_key = '2725326884-qM16ZMCOJw1Sdoi6jBrd0c6VAQKi4fykf8X5DaS',
-	                  access_token_secret= 'GlFQ0ZE3FDSiokSaVLj08qq0tmCw8sZjmwcai1oojJgbx')   
-	                  
-	r = api.request('statuses/user_timeline', {'count':2000, 'screen_name':user})
-	for item in r.get_iterator():
-		try:
-			item = item['text']
-			if item[0:2] == 'RT':
-				continue
-			else:
-				tweets.append(item)
-		except:
-			continue
-	return tweets
+class listener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["#hackbca"])
+
