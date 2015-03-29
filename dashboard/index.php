f1108a7e7c82915b786db9d71e01de54aaad0612
zachpanz88 <zachary@panzarino.com>
2015-03-29 06:05:26 -0400

    Title

--- dashboard/index.php
+++ dashboard/index.php
@@ -66,7 +66,7 @@
                 <div class="row" >
                     <div class="col-lg-12">
                         <h1 class="page-header" style="text-align:center;font-size:80px">
-                            Analyzus
+                            Analytics
                         </h1>
                     </div>
                 </div><br>
