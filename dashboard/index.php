25d31ce00be553ecb7567ba110355f504d22a7f4
zachpanz88 <zachary@panzarino.com>
2015-03-28 23:41:20 -0400

    Chart js

--- dashboard/index.php
+++ dashboard/index.php
@@ -92,14 +92,12 @@ function validateForm() {
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
+    
+    <!-- Chart.js -->
+    <script src="js/Chart.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
 
 </body>
 
