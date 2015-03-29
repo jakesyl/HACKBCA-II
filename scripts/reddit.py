c5947ea4af4497ca64e6534904768040e78f9a6b
Jake Sylvestre <me@jakesyl.com>
2015-03-29 01:30:38 -0400

    info

--- scripts/reddit.py
+++ scripts/reddit.py
@@ -1,49 +1,7 @@
-#!/usr/bin/env python 
-
-import requests 
-import json
-# TODO add this to a general query class 
-
-''''
-def example(self,user,get=1):
-	url = "http://www.reddit.com/user/" + user + "/overview.json"
-	res = requests.get(url)#get user overview
-	res.json()
-	y = json.loads(res.text)
-	return y
-'''
-
-def get_top(query, subreddit):
-	links = []
-	url="http://www.reddit.com/r/" + subreddit + "/search.json?q=" + query + "&sort=top&t=year"
-	res = requests.get(url)
-
-	res = json.loads(res.text)
-	#print res	
-	res =  res["data"]
-	res = res["children"]
-	print res 
-	for post in res:
-		data = []
-		post =  post["data"]
-		#post = post[0]
-		data.append(post["permalink"])
-		data.append(post["title"])
-		res.append(data)
-	#print res
+#python3!!!!
+import praw
 
 
-
-def get_comments(permalink):
-	#url = "http://www.reddit.com/" + permalink + 
-	pass
-#get permalink 
-
-#tests
-get_top("microsoft", "worldnews") 
+r = praw.Reddit(user_agent='analyz.us', log_requests=1)
 
 
-
-
-# send dates
-    # ADD MORE FUNCTIONALITY!!!
