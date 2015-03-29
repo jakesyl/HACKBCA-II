508e56cccab4feed5767ad74fe674d12733ee1b6
Alex <alexparson1@gmail.com>
2015-03-29 06:16:13 -0400

    symbol grab

--- dashboard/analyze.php
+++ dashboard/analyze.php
@@ -38,7 +38,13 @@ while($c<count($date)){
 }
 return $data;
 }
-echo gStock(array("9-9-2014","9-4-2014"),"MSFT");
+function getSymbol($name){
+$jsonData = file_get_contents("http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=".$name."&callback=YAHOO.Finance.SymbolSuggest.ssCallback");
+$jsonData = substr($jsonData, 39 ,-1);
+$data = json_decode($jsonData);
+//print_r($data);
+return $data->{'ResultSet'}->{'Result'}[0]->{'symbol'};
+}
 
 
 $reddit = preg_replace("/ /","+",$reddit);
